e9b12560c09b06eae5d8d882833c94edfd6beb74
Dhaval5353 <124448070+Dhaval5353@users.noreply.github.com>
2023-02-20 03:47:11 +0530

    Add files via upload

---
+++ Mx_player_bootstrap/script.js
@@ -0,0 +1,14 @@
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+  items: 5,
+  // items change number for slider display on desktop
+  nav: false,
+  dots: false,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 1400,
+  autoplayHoverPause: true
+});
+
